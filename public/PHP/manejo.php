75eb4c5716c9fd503b4c8d4fa9d25b3bc62737a7
Hector Saldivia <Hectorheck>
2021-10-20 22:20:41 -0300

    SOLUCION DE ERRORES 2

--- public/PHP/manejo.php
+++ public/PHP/manejo.php
@@ -5,17 +5,20 @@ function crearmatriz($Cnodos,$caminos)
 {
     $arreglo=array();
     $matriz=array();
-    
-    for($i=0;$i<$Cnodos;$i++)
-    {
-        array_push($arreglo,-1);                                
-    }
 
     for($i=0;$i<sizeof($caminos);$i++)
     {
-        $matriz[$caminos[$i]]=$arreglo;
+        $arreglo[$caminos[$i]]=-1;
+    }
+   
+
+    
+    for($i=0;$i<$Cnodos;$i++)
+    {
+        array_push($matriz,$arreglo);                                
     }
 
+   
     $matriz["Deterministico"] = true;
     return $matriz;
 
@@ -29,9 +32,8 @@ function crearmatriz($Cnodos,$caminos)
     function crearmatriz2($Cnodos, $conexiones, $caminos)    
     {
         $matriz = crearmatriz($Cnodos,$caminos);
-        echo sizeof($conexiones);
     
-       for($i=0;$i<sizeof($conexiones);$i++)
+        for($i=0;$i<sizeof($conexiones);$i++)
         {   
             if($matriz[$conexiones[$i][0]][$conexiones[$i][1]] == -1)
             {
@@ -40,23 +42,72 @@ function crearmatriz($Cnodos,$caminos)
             else
             {
                 $arreglo = array();
-
                 array_push($arreglo,$matriz[$conexiones[$i][0]][$conexiones[$i][1]]);
                 array_push($arreglo,$conexiones[$i][2]); 
                 $matriz[$conexiones[$i][0]][$conexiones[$i][1]] = $arreglo;
 
                 $matriz['Deterministico'] = false;
             }
-
+            
         }
-        
+       
     
         return $matriz;
     
     
    }
-           
 
+   
+   /*function afndtoafd($matriz,$caminos)
+   { 
+       $matriz2= array();
+       $queue = array(); 
+       array_push($matriz2,$matriz[0]);
+       for($i=0;$i<sizeof($caminos);$i++)
+       {
+            array_push($queue,$matriz[0][$caminos[$i]]);
+       }
+
+        while(empty($queue))
+        {
+            $aux = $queue[0];    
+                
+            if(is_array($aux))
+            {   
+                $name = '';
+                for($i=0;$i<sizeof($aux);$i++)
+                {
+                    $name = $name.$aux[$i];
+
+                    for($j=0;$j<sizeof($caminos);$j++)
+                    {   
+                        $camino = $matriz[$i][$caminos[$j]] ;
+                        array_push($queue,$camino);
+                        $matriz2[$name][$caminos[$j]] = $camino;  
+                    }
+                        
+                }
+
+            }
+            else
+            {
+                for($j=0;$j<sizeof($caminos);$j++)
+                {   
+                    $camino = $matriz[$aux][$caminos[$j]];
+                    array_push($queue,$camino);
+                    $matriz2[$name][$caminos[$j]] = $camino;  
+                }
+
+            }
+
+        }
+        array_splice($queue, 0);
+
+        return $matriz2;
+
+   }
+           
+*/
 
 
     
--- resources/views/Prueba2.php
+++ resources/views/Prueba2.php
@@ -2,9 +2,20 @@
 
 require("PHP\manejo.php");
 
-$conexiones[0]=['x',1,2];
-$conexiones[1] = ['x',1,3];
-print_r(crearmatriz2(5,$conexiones,["x","y","z"]));
+$conexiones[0]=[1,'x',2];
+$conexiones[1] = [2,'x',3];
+$conexiones[2] = [3,'y',1];
+$conexiones[3] = [4,'z',2];
+$conexiones[4] = [2,'x',5];
+$conexiones[5] = [0,'x',4];
+$conexiones[6] = [4,'x',3];
+$conexiones[7] = [1,'z',4];
+
+$matriz = crearmatriz2(5,$conexiones,["x","y","z"]);
+
+
+print_r($matriz);
+#print_r(afndtoafd($matriz,["x","y","z"]));
 
 
 
