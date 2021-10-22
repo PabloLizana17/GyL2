84b78c6a66e5ab56a400e38bf1e0e8553562be01
pablo <pablolizana0@gmail.com>
2021-10-22 18:03:56 -0300

    pequeños arreglos 2

--- resources/views/Prueba2.php
+++ resources/views/Prueba2.php
@@ -1,22 +1,22 @@
 <?php
 
-require("manejo.php");
+    require("manejo.php");
 
-$conexiones[0]=[1,'x',2];
-$conexiones[1] = [2,'x',3];
-$conexiones[2] = [2,'x',1];
-$conexiones[3] = [4,'z',2];
-$conexiones[4] = [2,'x',4];
-$conexiones[5] = [0,'x',4];
-$conexiones[6] = [4,'x',3];
-$conexiones[7] = [1,'z',4];
+    $conexiones[0]=[1,'x',2];
+    $conexiones[1] = [2,'x',3];
+    $conexiones[2] = [2,'x',1];
+    $conexiones[3] = [4,'z',2];
+    $conexiones[4] = [2,'x',4];
+    $conexiones[5] = [0,'x',4];
+    $conexiones[6] = [4,'x',3];
+    $conexiones[7] = [1,'z',4];
 
-$matriz = crearmatriz2(5,$conexiones,["x","y","z"]);
-
-
+    $matriz = crearmatriz2(5,$conexiones,["x","y","z"]);
 
 
-print_r(afndtoafd($matriz,["x","y","z"],2));
+
+
+    print_r(afndtoafd($matriz,["x","y","z"],2));
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('prueba2');
+    return view('prueba');
 });
