84d0488deb85e2d40a6923177bee41c88c45de92
Vicente Velasquez <vicho.velasquez40@gmail.com>
2021-10-24 20:21:28 -0300

    Despliegue de resultados obtenidos

--- resources/views/Prueba2.php
+++ resources/views/Prueba2.php
@@ -20,7 +20,8 @@
     $union = union($automata2['Cnodos'],$lineas2['conexiones'],$lineas2['caminos'],$automata2['inicio'],$automata2["Fin"],$automata1['Cnodos'],$lineas1['conexiones'],$lineas1['caminos'],$automata1['inicio'],$automata1["Fin"]);
     $interseccion = interseccion($automata2['Cnodos'],$lineas2['conexiones'],$lineas2['caminos'],$automata2['inicio'],$automata2["Fin"],$automata1['Cnodos'],$lineas1['conexiones'],$lineas1['caminos'],$automata1['inicio'],$automata1["Fin"]);
     $concatenacion = concatenacion($automata2['Cnodos'],$lineas2['conexiones'],$lineas2['caminos'],$automata2['inicio'],$automata2["Fin"],$automata1['Cnodos'],$lineas1['conexiones'],$lineas1['caminos'],$automata1['inicio'],$automata1["Fin"]);
-
+    $complemento1 = complemento($automata1['Cnodos'],$lineas1['conexiones'],$lineas1['caminos'],$automata1['inicio'],$automata1['Fin']);
+    $complemento2 = complemento($automata2['Cnodos'],$lineas2['conexiones'],$lineas2['caminos'],$automata2['inicio'],$automata2['Fin']);
 
     
 
